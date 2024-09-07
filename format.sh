3992515d83a1d582c0da518a79d6a46726309f9b
Dirk Zimmermann <dz@planck.security>
2024-09-07 14:21:46 +0200

    Try clang format 17

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-clang-format-mp-18 -i botan/src/lib/rng/jitter_rng/jitter*
-clang-format-mp-18 -i botan/src/tests/test_jitter_rng.cpp
+clang-format-mp-17 -i botan/src/lib/rng/jitter_rng/jitter*
+clang-format-mp-17 -i botan/src/tests/test_jitter_rng.cpp
